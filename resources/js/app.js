c849394038e7e0540d725bcbc212fc6f497229cd
Aryeh Raber <aryeh.raber@gmail.com>
2024-11-10 15:19:21 +0100

    Delete `ExampleComponent`

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,6 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('clients-list', require('./components/ClientsList.vue').default);
 Vue.component('client-form', require('./components/ClientForm.vue').default);
 Vue.component('client-show', require('./components/ClientShow.vue').default);
